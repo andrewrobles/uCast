810ee9c2ef1c85f3ea8a96617a8cc3bb4f4c2443
Andrew Robles <me@andrewrobles.com>
2021-03-26 04:09:41 -0700

    Add uCast image

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,13 @@
 import Head from 'next/head'
 import 'bootstrap/dist/css/bootstrap.css'
 import styles from '../styles/Home.module.css'
+import Image from 'next/image'
 
 
 export default function Home() {
   return (
     <div className={styles.container}>
+     <Image src='/uCast.png' width='64' height='68'/>
      <h1 className={styles.title}>uCast</h1>
      <p className={styles.description}>Provide an email to be notified when we launch</p>
     </div>
