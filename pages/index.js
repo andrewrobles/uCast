788dfcf61dbaaccda655a9a1d890d2e338f577b3
Andrew Robles <me@andrewrobles.com>
2021-03-26 05:47:57 -0700

    Alert user when they submit their email

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,25 @@
-import Head from 'next/head'
+import { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.css'
 import styles from '../styles/Home.module.css'
 import Image from 'next/image'
 
 
 export default function Home() {
+  // Store email in state variable
+  const [email, setEmail] = useState('');
+
+  const handleSubmit = () => { 
+    // Alert user when they submit their email
+    const message = "A notification will be sent to " + email + " when we officially launch"
+    alert(message) 
+  }
+
+  
+  const handleChange = (event) => {
+    // Update email state with contents of input field
+    setEmail(event.target.value)
+  }
+
   return (
     <div className={styles.container}>
      <Image src='/uCast.png' width='64' height='68'/>
@@ -12,9 +27,9 @@ export default function Home() {
      <div class='form-group'>
      <p className={styles.description}>Provide an email to be notified when we launch</p>
       <div class='input-group'>
-        <input className='form-control' placeholder="Email address"/>
+        <input className='form-control' onChange={ handleChange } placeholder="Email address"/>
         <div class='input-group-append'>
-          <button className='input-group-text'>Submit</button>
+          <button className='input-group-text' onClick={ handleSubmit }>Submit</button>
         </div>
       </div>
      </div>
